26235968e6659446e388e085d9137d1493bbb467
ahmedvlack <153669023+ahmedvlack@users.noreply.github.com>
2024-08-23 06:54:36 +0300

    Delete Plugins directory

--- Plugins/ارفعنى.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, isAdmin }) => {
-  if (m.fromMe) throw 'Nggk'
-  if (isAdmin) throw `*انت ادمن زعيم المكان يا مطورى 🌚♥️*`
-  await conn.groupParticipantsUpdate(m.chat, [m.sender], "promote")
-}
-handler.command = /^ارفع|ارفعنى|ار|ارفعني|autoadmin$/i
-handler.rowner = true
-handler.botAdmin = true
-export default handler
